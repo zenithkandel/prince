0df0a68dcdd961756f554fcebf92505b8b416205
Zenith Kandel <kandelze123@gmail.com>
2026-04-19 08:17:17 +0545

    Remove unused files and restore footer contact section in index.php

--- prince-a5d1ae3adea285a63d79de107856d026331f5d02/prince-a5d1ae3adea285a63d79de107856d026331f5d02/api/get.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-
-$json_file = 'data.json';
-if (file_exists($json_file)) {
-    echo file_get_contents($json_file);
-} else {
-    echo json_encode(['error' => 'Data not found']);
-}
